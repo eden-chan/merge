0ddaaf551a1853c15da9958ba9ab08784d807a85
Eden Chan <edenchan717@gmail.com>
2024-12-05 16:56:21 -0500

    use card component

---
+++ quartz/components/data/simpleCards.ts
@@ -0,0 +1,18 @@
+export const simpleCardsData = {
+  basics: {
+    title: "General Grants",
+    href: "/basics"
+  },
+  fellowships: {
+    title: "Fellowship Programs",
+    href: "/fellowships"
+  },
+  sectorGrants: {
+    title: "Sector/Geo Grants",
+    href: "/sector-grants"
+  },
+  resources: {
+    title: "Resources",
+    href: "/resources"
+  }
+} 
